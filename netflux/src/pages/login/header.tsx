63e5454bd593546dc54d056c57a42dc0ab686f58
OnyxXs <timeo.donnaes@gmail.com>
2023-10-26 15:52:26 +0200

    update header.css and header.tsx

--- netflux/src/pages/login/header.tsx
+++ netflux/src/pages/login/header.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import './header.css';
 
 const Header: React.FC = () => {
   return (
     <header>
-      <h1>Mon Site Web</h1>
-      <nav>
-        <ul>
-          <li><a href="page1.html">Page 1</a></li>
-          <li><a href="page2.html">Page 2</a></li>
-          {/* Ajoutez d'autres liens de navigation au besoin */}
-        </ul>
-      </nav>
+        <nav className="navbar">
+            <div className="image">
+                <img src="https://cdn.discordapp.com/attachments/1166370986355064844/1167096613408342127/AddText_10-25-03.21.23_1.png?ex=654ce26c&is=653a6d6c&hm=e4d407e6ccfef0d592f1c0e8dfdc532057fadb389ac75fc31db01f7758baf390&" alt="Image navbar"/>
+            </div>
+            <div className="divnav">
+                <a href="#" className="correctlink">Menu</a>
+            </div>
+        </nav>
     </header>
   );
 };
