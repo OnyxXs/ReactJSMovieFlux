9c1903c5f96e6319fd0226568cef4e8b8007ea8c
OnyxXs <timeo.donnaes@gmail.com>
2023-10-26 16:04:03 +0200

    update header.css and header.tsx

--- netflux/src/pages/login/header.tsx
+++ netflux/src/pages/login/header.tsx
@@ -9,7 +9,16 @@ const Header: React.FC = () => {
                 <img src="https://cdn.discordapp.com/attachments/1166370986355064844/1167096613408342127/AddText_10-25-03.21.23_1.png?ex=654ce26c&is=653a6d6c&hm=e4d407e6ccfef0d592f1c0e8dfdc532057fadb389ac75fc31db01f7758baf390&" alt="Image navbar"/>
             </div>
             <div className="divnav">
-                <a href="#" className="correctlink">Menu</a>
+
+                <div className="dropdown">
+                    <button className="dropdown-btn">Menu</button>
+                    <div className="dropdown-content">
+                        <a href="#">Page 1</a>
+                        <a href="#">Page 2</a>
+                        <a href="#">Page 3</a>
+                    </div>
+                </div>
+
             </div>
         </nav>
     </header>
