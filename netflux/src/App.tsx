c7b04f7d46803a88a904d7f2399d1abb296da8cd
OnyxXs <timeo.donnaes@gmail.com>
2023-10-27 10:15:57 +0200

    fix branch

---
+++ netflux/src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RegisterAndlogin from "./pages/login/registerAndLogin";
+import HomeScreen from "./pages/login/home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<RegisterAndlogin />} />
+          <Route path="/home" element={<HomeScreen />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
