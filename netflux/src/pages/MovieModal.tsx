57d7744aee0d5c67ab2b70cbc4bb1195a6ad1a80
YimoSsDev <116626946+YimoSsDev@users.noreply.github.com>
2023-10-27 10:30:53 +0200

    Update 2.5 (Commentaires et avis)

--- netflux/src/pages/MovieModal.tsx
+++ netflux/src/pages/MovieModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MovieModal.css";
 
 interface Movie {
@@ -10,12 +10,42 @@ interface Movie {
   seasons?: number;
 }
 
+interface UserComment {
+  rating: number;
+  comment: string;
+  userName: string;
+}
+
 interface MovieModalProps {
   movie: Movie;
   onClose: () => void;
 }
 
 function MovieModal({ movie, onClose }: MovieModalProps) {
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState("");
+  const [userComments, setUserComments] = useState<UserComment[]>([]);
+
+  const handleRatingChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRating(parseInt(event.target.value));
+  };
+
+  const handleCommentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(event.target.value);
+  };
+
+  const handleCommentSubmit = () => {
+    const newComment: UserComment = {
+      rating,
+      comment,
+      userName: "Nom de l'utilisateur",
+    };
+    setUserComments([...userComments, newComment]);
+
+    setRating(0);
+    setComment("");
+  };
+
   return (
     <div className="movie-modal">
       <div className="modal-background">
@@ -37,6 +67,39 @@ function MovieModal({ movie, onClose }: MovieModalProps) {
           {movie.seasons && (
             <p className="movie-seasons">Seasons: {movie.seasons}</p>
           )}
+
+          <div className="user-comments">
+            <h3>User Comments</h3>
+            <div className="comments-list">
+              {userComments.map((userComment, index) => (
+                <div key={index} className="user-comment">
+                  <p>{userComment.userName}:</p>
+                  <p>Rating: {userComment.rating}</p>
+                  <p>Comment: {userComment.comment}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="comment-form">
+            <h3>Leave a Comment</h3>
+            <label htmlFor="rating">Rating:</label>
+            <input
+              type="number"
+              id="rating"
+              min="0"
+              max="10"
+              value={rating}
+              onChange={handleRatingChange}
+            />
+            <label htmlFor="comment">Comment:</label>
+            <textarea
+              id="comment"
+              value={comment}
+              onChange={handleCommentChange}
+            />
+            <button onClick={handleCommentSubmit}>Submit</button>
+          </div>
         </div>
       </div>
     </div>
